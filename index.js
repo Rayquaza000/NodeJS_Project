60b06bce2b919c257489a9898f36f1603d48ed31
Rayquaza000 <mehtapratik2001@gmail.com>
2025-09-18 18:39:58 +0530

    Almost completed

--- index.js
+++ index.js
@@ -33,13 +33,14 @@ async function checkLogin(req,res,next){
     const userByEmail=await user.find({email:req.body.email});
     if(userByEmail.length>0)
     {
-        if(userByEmail.loggedIn==true)
+        if(userByEmail[0].loggedIn==true)
         {
+            
             return res.json({"message":"User is already logged in"});
         }
         else 
             {
-                if(userByEmail.password==req.body.password)
+                if(userByEmail[0].password==req.body.password)
                     {
                        next();
                     }
@@ -53,6 +54,24 @@ async function checkLogin(req,res,next){
     }
 }
 
+async function checkLoginStatus(req,res,next){
+    const userByEmail=await user.find({email:req.body.email});
+    if(userByEmail.length>0)
+    {
+        if(userByEmail[0].loggedIn==true)
+        {
+            
+            next();
+        }
+        else 
+            {
+                return res.json({"message":"user is logged out"})
+            }
+    }
+    else{
+        return res.json({"message":"User not registered."})
+    }
+}
 
 app.get("/products",async (req,res)=>{
     const products=await product.find();
@@ -71,11 +90,14 @@ app.get("/product/:id",async (req,res)=>{
     
 });
 
+
+
 app.post("/register",userExists,(req,res)=>{
 
     const newUser=new user({
-    firstName:req.body.firstname,
-    lastName:req.body.lastname,
+    userId:req.body.userId,
+    firstName:req.body.firstName,
+    lastName:req.body.lastName,
     email:req.body.email,
     password:req.body.password,
     mobile:req.body.mobile,
@@ -86,12 +108,55 @@ app.post("/register",userExists,(req,res)=>{
     res.json(newUser)
 });
 
-app.post("/login",checkLogin,(req,res)=>{
-    const updateLoggedIn=user.findByIdAndUpdate(
-                        req.body.email,
+app.post("/login",checkLogin,async (req,res)=>{
+    const updateLoggedIn=await user.findOneAndUpdate(
+                        {email:req.body.email},
                         {loggedIn:true}
                         );
-    return res.json({"message":"Login successful"});
+    const loggedin_users_info=await user.find({"email":req.body.email});
+    console.log(loggedin_users_info[0]);
+    const accessToken=jwt.sign(loggedin_users_info[0]["email"],"secretkey");
+    res.send({"token":accessToken,"message":"Login successful"});
+    
+});
+
+app.post("/logout",checkLoginStatus, async (req,res)=>{
+    const updateLoggedIn=await user.findOneAndUpdate({email:req.body.email},{loggedIn:false});
+
+});
+
+app.post("/cart",checkLoginStatus,async (req,res)=>{
+    const productId=req.body.productId;
+    const findProductIdInDB=await product.find({productId:productId});
+    if(findProductIdInDB.length>0 && findProductIdInDB.quantity>0)
+    {
+        const checkIfProductAlreadyExists=await cart.find({userId:req.body.userId,productId:productId});
+        if(checkIfProductAlreadyExists==undefined){
+        const newItem=new cart({
+            userId:req.body.userId,
+            productId:req.body.productId,
+            quantity:1
+        });
+        newItem.save();
+        return res.send({"message":"item added to cart"})}
+        else{
+        await cart.findOneAndUpdate({userId:req.body.userId,productId:productId},{quantity:checkIfProductAlreadyExists[0].quantity+1});
+            return res.send({"message":"item already exists in cart. Increased quantity by one"});
+    }
+    }
+    
+    else{
+        return res.json({"message":"This product is not available"});
+    }
+});
+
+app.put("/cart",checkLoginStatus,async (req,res)=>{
+    const updatedProductQuantity=findOneAndUpdate({userId:req.body.userId,productId:req.body.productId},{quantity:req.body.quantity},{new:true});
+    return res.json({"updatedProductQuantity":updatedProductQuantity})
+});
+
+app.delete("/cart",(req,res)=>{
+
 });
 
 app.listen(5100,()=>{
